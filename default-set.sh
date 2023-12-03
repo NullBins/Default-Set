5d70063ee586532cdd282e77770ee0ce259e7ad4
Donghyun Choi <101495051+NullBins@users.noreply.github.com>
2023-12-03 22:50:28 +0900

    Update default-set.sh

--- default-set.sh
+++ default-set.sh
@@ -3,34 +3,42 @@
 # - COLORS - #
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-BLUE='\033[0;34m'
+BLUE='\033[0;36m'
+NONE='\\033[0m'
 
 # - MAIN - #
-echo -e "${BLUE}|START| Start basic setup.."
-sleep 5s
-echo -e "${GREEN}|No.1| APT Updating & Install required packages."
+echo -e "${BLUE}|START| Start basic setup..${NONE}"
+sleep 3s
+echo -e "${GREEN}|No.1| APT Updating & Install required packages.${NONE}"
+sleep 3s
 sed -i "5,6s/^/# /g" /etc/apt/sources.list
 apt update -y
 apt install -y vim net-tools dnsutils tcpdump curl lynx wget ssh tcptraceroute traceroute nfs-common smbclient psmisc sudo
 sed -i "s/\"syntax on/syntax on\nset number\nset ignorecase/g" /etc/vim/vimrc
-echo -e "${GREEN}|No.2| Add /etc/resolv.conf"
+echo -e "${GREEN}|No.2| Add /etc/resolv.conf${NONE}"
+sleep 3s
 echo "nameserver 127.0.0.1" > /etc/resolv.conf
-echo -e "${GREEN}|No.3| Enable to port forwarding."
+echo -e "${GREEN}|No.3| Enable to port forwarding.${NONE}"
+sleep 3s
 sed -i "28s/^#//g" /etc/sysctl.conf
 sed -i "33s/^#//g" /etc/sysctl.conf
 sysctl -p
-echo -e "${GREEN}|No.4| Setting for /etc/ssh/sshd_config"
+echo -e "${GREEN}|No.4| Setting for /etc/ssh/sshd_config${NONE}"
+sleep 3s
 sed -i "s/#Port/Port/g" /etc/ssh/sshd_config
 sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
 sed -i "s/#Banner none/Banner \/etc\/issue/g" /etc/ssh/sshd_config
 systemctl restart sshd
-echo -e "${GREEN}|No.5| Configure banner & motd."
+echo -e "${GREEN}|No.5| Configure banner & motd.${NONE}"
+sleep 3s
 cat /dev/null > /etc/issue
 echo -e "\nWelcome to Linux Environments (IT Network Systems Administration)\n" > /etc/issue
 cp /etc/motd /etc/motd.bak
 cat /dev/null > /etc/motd
-echo -e "${GREEN}|No.6| Configure timezone to Asia/Seoul"
+echo -e "${GREEN}|No.6| Configure timezone to Asia/Seoul${NONE}"
+sleep 3s
 timedatectl set-timezone Asia/Seoul
 hwclock -w
 timedatectl status
-echo -e "${BLUE}|Finished| Complete Setup!"
+echo -e "${BLUE}MFA: |Finished| Complete Setup!${NONE}"
+# - - - #
